712d197a9cddba3e33d760a79eb2b39799880ed4
Devi <76871832+mangaladevis@users.noreply.github.com>
2022-12-18 09:16:29 +0000

    Create API to convert from dat format to json format
    Created new API which convert the given dat format to json format

---
+++ converter-parent/converter-api/src/main/java/com/bubbles/learning/ApiController.java
@@ -0,0 +1,27 @@
+package com.bubbles.learning;
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bubbles.learning.converter.DatToJsonConverter;
+
+
+@RestController
+public class ApiController {
+
+	@PostMapping("/converter")
+	public ResponseEntity<String> convert(@RequestBody String requestBody) {
+		List<String> toConvert = requestBody.lines().toList();
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<String>(DatToJsonConverter.convert(toConvert),responseHeaders,HttpStatus.OK);
+	}
+
+}
